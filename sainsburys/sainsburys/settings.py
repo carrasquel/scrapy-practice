49b4104e54c7f973ab6699cd040765b1df62bd85
Nelson Carrasquel <carrsquel@outlook.com>
2021-08-17 00:29:59 -0400

    tests

--- sainsburys/sainsburys/settings.py
+++ sainsburys/sainsburys/settings.py
@@ -17,7 +17,7 @@ NEWSPIDER_MODULE = 'sainsburys.spiders'
 USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 CONCURRENT_REQUESTS = 1
--- sainsburys/sainsburys/spiders/basic.py
+++ sainsburys/sainsburys/spiders/basic.py
@@ -1,26 +1,31 @@
+from urllib.parse import urljoin
+
 import scrapy
 from scrapy import Request
+
+from sainsburys.items import SainsburysItem
 
 
 class BasicSpider(scrapy.Spider):
     name = 'basic'
-    allowed_domains = ['https://www.sainsburys.co.uk/shop/gb/groceries/meath-fish/']
-    start_urls = ['https://www.sainsburys.co.uk/shop/gb/groceries/meath-fish//']
+    allowed_domains = ['www.sainsburys.co.uk']
+    start_urls = ['https://www.sainsburys.co.uk/shop/gb/groceries/meat-fish']
 
     def parse(self, response):
-        
         
         urls = response.css("ul.categories.departments li a::attr(href)").extract()
 
         for url in urls:
-
+            
             yield response.follow(url, callback=self.parse_department)
+            # yield Request(urljoin(response.url, url), callback=self.parse_department)
 
     def parse_department(self, response):
 
         products = response.css("ul.productLister.gridView").extract()
 
         if products:
+
             for product in self.handle_product_listing(response):
                 yield product
         
@@ -31,7 +36,7 @@ class BasicSpider(scrapy.Spider):
 
         if not pages:
             return
-
+        
         for url in pages:
             yield response.follow(url, callback=self.parse_department)
 
@@ -49,4 +54,21 @@ class BasicSpider(scrapy.Spider):
 
     def parse_product(self, response):
 
-        pass
+        print()
+        print(response.url)
+        print()
+
+        product_name = response.css("h1.pd__header::attr(text)").extract()
+        product_image = response.css("img.pd__image::attr(src)").extract()
+
+        print()
+        print(product_name, product_image)
+        print()
+
+        item = SainsburysItem()
+
+        item["url"] = response.url
+        item["name"] = product_name
+        item["image"] = product_image
+
+        yield item
